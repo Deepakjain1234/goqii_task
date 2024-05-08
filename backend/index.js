52ac62e57c6a75c1ff99f1b870438736e544c274
deepak jain <deeepakjain132@gmail.com>
2024-05-08 09:56:25 +0530

    cors add

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const express = require('express')
 var bodyParser = require('body-parser')
-
+const cors = require('cors');
 const hostname = '127.0.0.1';
 
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cors());
 // parse application/json
 app.use(bodyParser.json())
 // sequelize.sync({force: true});
